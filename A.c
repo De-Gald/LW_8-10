4fe3737948a0a498f9b39ad828694cbcca4f22b3
De-Gald <54669029+De-Gald@users.noreply.github.com>
2021-01-26 19:04:46 +0200

    Update A.c

--- A.c
+++ A.c
@@ -45,8 +45,7 @@ int main() {
         close(fd);
         close(fd2);
         
-        fd = 0;
-        fd2 = 0;
+        sleep(1);
 
         fd2 = open("B >> A", O_WRONLY);
         fd = open("C >> A", O_WRONLY);
